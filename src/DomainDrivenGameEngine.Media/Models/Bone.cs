d8d819ba5bf0330df7e4512f3c1853b36948ab4d
Preston Chaderton <pchaderton@gmail.com>
2020-11-28 11:34:28 -0500

    Updating Bone to recursively compute world-to-bind matrix at construction time for the convenience of external implementations.

--- src/DomainDrivenGameEngine.Media/Models/Bone.cs
+++ src/DomainDrivenGameEngine.Media/Models/Bone.cs
@@ -16,9 +16,11 @@ namespace DomainDrivenGameEngine.Media.Models
         /// <param name="offsetMatrix">The matrix to use for offsetting this bone from the parent.</param>
         /// <param name="name">The name of the bone.</param>
         /// <param name="children">The children of this bone.  The bones in this collection will have their parent set to this bone.</param>
+        /// <param name="computeWorldToBindMatrices">Optional, defaults to <c>false</c>.  When <c>true</c>, computes the world-to-bind matrices for this bone and all child bones.</param>
         public Bone(Matrix4x4 offsetMatrix,
                     string name,
-                    ReadOnlyCollection<Bone> children = null)
+                    ReadOnlyCollection<Bone> children = null,
+                    bool computeWorldToBindMatrices = false)
         {
             OffsetMatrix = offsetMatrix;
             Name = name ?? throw new ArgumentNullException(nameof(name));
@@ -28,6 +30,11 @@ namespace DomainDrivenGameEngine.Media.Models
             foreach (var child in Children)
             {
                 child.Parent = this;
+            }
+
+            if (computeWorldToBindMatrices)
+            {
+                ComputeWorldToBindMatrix(Matrix4x4.Identity);
             }
         }
 
@@ -50,5 +57,28 @@ namespace DomainDrivenGameEngine.Media.Models
         /// Gets the parent of this bone.
         /// </summary>
         public Bone Parent { get; private set; }
+
+        /// <summary>
+        /// Gets the matrix used to transform vertices attached to this bone from world space to bind space.
+        /// </summary>
+        public Matrix4x4 WorldToBindMatrix { get; private set; }
+
+        /// <summary>
+        /// Computes the world-to-bind matrix for this bone based on a parent bone offset matrix.
+        /// </summary>
+        /// <param name="parentGlobalOffsetMatrix">The <see cref="Matrix4x4"/> which represents the bone parent's total offset matrix.</param>
+        private void ComputeWorldToBindMatrix(Matrix4x4 parentGlobalOffsetMatrix)
+        {
+            var globalOffsetMatrix = parentGlobalOffsetMatrix * OffsetMatrix;
+
+            Matrix4x4.Invert(globalOffsetMatrix, out var worldToBindMatrix);
+
+            WorldToBindMatrix = worldToBindMatrix;
+
+            foreach (var child in Children)
+            {
+                child.ComputeWorldToBindMatrix(globalOffsetMatrix);
+            }
+        }
     }
 }
